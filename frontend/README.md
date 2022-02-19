f06b90bfd4e13bf4adf44b1ba151713e672eecb0
frontendprof <abdumalksharipov@gmail.com>
2022-02-19 12:57:32 +0500

    backend && mongo config

--- frontend/README.md
+++ frontend/README.md
@@ -6,3 +6,8 @@
 
 1. Wiring up to github account
 2. Importing it from vercel
+
+### Tools to use
+
+1. Mongodb as database
+2. GraphQL for pulling and pushing data to/from db and different mutations
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import NProgress from 'nprogress';
+import Router from 'next/router';
+// import Page from '../components/Page';
+import '../styles/nprogress.css';
+
+import '../styles/globals.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
